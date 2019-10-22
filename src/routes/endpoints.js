75ae8504f1742f4a33a233a04ebdbac1a4918bf2
Alex Pariona <lx7pary@gmail.com>
2019-10-22 15:14:51 -0500

    change api

--- src/routes/endpoints.js
+++ src/routes/endpoints.js
@@ -7,14 +7,14 @@ const initializeEndPoints = (app) => {
 
     //sunat
     app.post("/public/api/sunat/ruc", consulta.searchMultiRuc); 
-    app.get("/public/api/sunat/ruc/:ruc", consulta.searchRuc);
+    app.get("/public/api/ruc/:ruc", consulta.searchRuc);
     app.get("/public/api/sunat/dni/:dni", consulta.searchSunatDni);
     app.get("/public/api/tc-sunat/actual", consulta.getSunatTipoCambioActual);
     app.get("/public/api/tc-sunat/:year/:month", consulta.getSunatTipoCambio);
     app.get("/public/api/tc-sunat/:year/:month/:day", consulta.getSunatTipoCambioPorDia);
   
     //jne
-    app.get("/public/api/jne/dni/:dni", consulta.searchDni); 
+    app.get("/public/api/dni/:dni", consulta.searchDni); 
 
     //essalud
     app.get("/public/api/essalud/dni/:dni", consulta.searchEssaludDni); 
